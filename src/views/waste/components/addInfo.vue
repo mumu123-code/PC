9e7488f26e06183ce3dd8e355721f0c4e7e19eac
zhang1996 <943087558@qq.com>
2022-06-17 16:25:20 +0800

    z

--- src/views/waste/components/addInfo.vue
+++ src/views/waste/components/addInfo.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="addInfoCon">
-    信息填写
+    <div class="title">
+        配置信息
+    </div>
+    <el-card>
+        <div class="card-title">
+            配置信息填写
+        </div>
+    </el-card>
+     <el-card>
+        <div class="card-title">
+            配置信息列表
+        </div>
+    </el-card>
   </div>
 </template>
 
@@ -15,5 +27,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.addInfoCon{
+    .title{
+        text-align: center;
+        font-size: 16px;
+        font-weight: bold;
+        letter-spacing:5px;
+        margin-bottom: 30px;
+    }
+    .card-title{
+        color: rgb(131, 130, 130);
+        border-bottom: 2px solid #eee;
+    }
+}
 </style>
