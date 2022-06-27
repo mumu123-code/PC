2c54ad87406726b785ba419eabc237f7019c9f86
qi <695153701@qq.com>
2022-06-27 15:47:47 +0800

    x

--- src/views/standing/consumablesChart.vue
+++ src/views/standing/consumablesChart.vue
@@ -8,28 +8,28 @@
       <div class="creatTime">创建时间：{{ time }}</div>
 
       <el-table :data="consumablesData" style="width: 100%,margon-top:20px" :header-cell-style="{'background':'#F5F3F2'}">
-        <el-table-column prop="consumablesData.consumablesType" label="耗材种类" width="200">
+        <el-table-column prop="consumablesType" label="耗材种类" width="200">
           <template slot-scope="scope">
             <el-input v-model="scope.row.consumablesType" placeholder="请输入"></el-input>
           </template>
         </el-table-column>
-        <el-table-column prop="consumablesData.replacementAmount" label="更换量(kg)" width="220">
+        <el-table-column prop="replacementAmount" label="更换量(kg)" width="220">
           <template slot-scope="scope">
             <el-input v-model="scope.row.replacementAmount" placeholder="请输入"></el-input>
           </template>
         </el-table-column>
-        <el-table-column prop="consumablesData.replacementTime" label="更换时间" width="260">
+        <el-table-column prop="replacementTime" label="更换时间" width="260">
           <template slot-scope="scope">
             <el-date-picker v-model="scope.row.replacementTime" type="date" value-format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期"></el-date-picker>
           </template>
         </el-table-column>
-        <el-table-column prop="consumablesData.handleType" label="处置情况" width="300">
+        <el-table-column prop="handleType" label="处置情况" width="300">
           <template slot-scope="scope">
             <el-radio :label="1" v-model="scope.row.handleType" @change.native="getCurrentRow(scope.row)">危废间暂存</el-radio>
             <el-radio :label="2" v-model="scope.row.handleType" @change.native="getCurrentRow(scope.row)">委外处置</el-radio>
           </template>
         </el-table-column>
-        <el-table-column prop="consumablesData.handleTime" label="处置时间">
+        <el-table-column prop="handleTime" label="处置时间">
           <template slot-scope="scope">
             <el-date-picker v-model="scope.row.handleTime" value-format="yyyy-MM-dd HH:mm:ss" type="date" placeholder="选择日期"></el-date-picker>
           </template>
